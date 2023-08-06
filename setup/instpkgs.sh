97f7e5a3cef1c2e82e4290ca7c801e62d72ff355
John Goerzen <jgoerzen@complete.org>
2023-08-06 18:33:35 -0500

    bookworm fix

--- setup/instpkgs.sh
+++ setup/instpkgs.sh
@@ -4,7 +4,7 @@ set -x
 
 apt-get update
 apt-get  -y -u install git gcc make wget libncurses-dev sharutils zip unzip telnet bc bison flex libssl-dev sudo python3-pexpect \
-        expect libpcap-dev libvdeplug-dev \
+        expect libpcap-dev libvdeplug-dev bzip2 \
         simh \
         colossal-cave-adventure open-adventure bsdgames \
         fortunes fortune-mod fortunes-bofh-excuses figlet sysvbanner bsdmainutils gopher alpine ftp telnet \
